982b34eb2ec5b92c4d9d8f0f1d550765627f1c09
Andrew Dymond <andy.dymond@gmail.com>
2018-02-02 12:46:36 -0700

    Update location show to show at least 2 specials

--- spec/features/user_sees_locations_spec.rb
+++ spec/features/user_sees_locations_spec.rb
@@ -16,4 +16,14 @@ describe "user can visit locations" do
     expect(page).to have_content("Live in Denver")
     expect(page).to have_content("Live in Denver Again")
   end
+
+  it "doesn't display specials if there are less than two" do
+    Comedian.create(name: "Joe", age: 23)
+    Location.create(name: "Denver")
+    Special.create(name: "Live in Denver", comedian_id: 1, location_id: 1)
+
+    visit("/locations/1")
+
+    expect(page).to have_content("Not enough specials!")
+  end
 end
